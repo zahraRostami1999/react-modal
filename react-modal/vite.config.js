7101e64beb497330fccbbf6b20fea1d02dd512c7
Zahra Rostami <zahrarostami137878@gmail.com>
2025-05-16 12:11:39 +0330

    initialize

---
+++ react-modal/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  server: {
+    host: "127.0.0.1",
+    port: 40000,
+  }
+})
